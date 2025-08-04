517e6c5b29c126921a0f59a04f831fed62dde300
simple-colour <simply.known.colour@gmail.com>
2025-08-04 14:57:29 +1000

    Add files via upload

--- utility_menu_v5.lua
+++ utility_menu_v5.lua
@@ -1,4 +1,6 @@
 
+-- #$%#$% SETTINGS #$%#$%
+
 if not istable(settings) then
 	settings = {
 		propbox = false,
@@ -7,37 +9,62 @@ if not istable(settings) then
 		npcnametag = false,
 		playernametag = false,
 		speedometer = false,
-		autobhop = false
+		autobhop = false,
+		npccursorline = false,
+		playercursorline = false
 	}
 end
 
+-- #$%#$% HOOKS #$%#$%
+
 hook.Add("PostDrawOpaqueRenderables", "DrawEntityBoxes", function()
+	local allents = ents.GetAll()
 	local ply = LocalPlayer()
-	for _, ent in ipairs(ents.GetAll()) do
-		if settings.propbox and (ent:GetClass():find("prop_physics") or ent:GetClass():find("prop_dynamic")) then
-			render.DrawWireframeBox(ent:GetPos(), ent:GetAngles(), ent:OBBMins(), ent:OBBMaxs(), Color(0, 255, 255), false)
-		elseif settings.npcbox and ent:IsNPC() and ent:Alive() then
-			render.DrawWireframeBox(ent:GetPos(), Angle(0, 0, 0), ent:OBBMins(), ent:OBBMaxs(), Color(255, 0, 0), false)
-		elseif settings.playerbox and ent:IsPlayer() and ent ~= ply and ent:Alive() then
-			render.DrawWireframeBox(ent:GetPos(), Angle(0, 0, 0), ent:OBBMins(), ent:OBBMaxs(), Color(255, 255, 0), false)
+	if settings.propbox then
+		for _, ent in ipairs(allents) do
+			if ent:GetClass():find("prop_physics") or ent:GetClass():find("prop_dynamic") then
+				render.DrawWireframeBox(ent:GetPos(), ent:GetAngles(), ent:OBBMins(), ent:OBBMaxs(), Color(0, 255, 255), false)
+			end
+		end
+	end
+	if settings.npcbox then
+		for _, ent in ipairs(allents) do
+			if ent:IsNPC() and ent:Alive() then
+				render.DrawWireframeBox(ent:GetPos(), Angle(0, 0, 0), ent:OBBMins(), ent:OBBMaxs(), Color(255, 0, 0), false)
+			end
+		end
+	end
+	if settings.playerbox then
+		for _, ent in ipairs(allents) do
+			if ent:IsPlayer() and ent ~= ply and ent:Alive() then
+				render.DrawWireframeBox(ent:GetPos(), Angle(0, 0, 0), ent:OBBMins(), ent:OBBMaxs(), Color(255, 255, 0), false)
+			end
 		end
 	end
 end)
 
 hook.Add("HUDPaint", "DrawNametags", function()
+	local allents = ents.GetAll()
 	local ply = LocalPlayer()
-	for _, ent in ipairs(ents.GetAll()) do
-		if settings.npcnametag and ent:IsNPC() and ent:Alive() then
-			local pos = ent:EyePos():ToScreen()
-			draw.SimpleText(ent:GetClass(), "BudgetLabel", pos.x, pos.y, Color(255, 0, 0), TEXT_ALIGN_CENTER, TEXT_ALIGN_BOTTOM)
-		elseif settings.playernametag and ent:IsPlayer() and ent ~= ply and ent:Alive() then
-			local pos = ent:EyePos():ToScreen()
-			draw.SimpleText(ent:Nick(), "BudgetLabel", pos.x, pos.y, Color(255, 255, 0), TEXT_ALIGN_CENTER, TEXT_ALIGN_BOTTOM)
+	if settings.npcnametag then
+		for _, ent in ipairs(allents) do
+			if ent:IsNPC() and ent:Alive() then
+				local pos = ent:EyePos():ToScreen()
+				draw.SimpleText(ent:GetClass(), "BudgetLabel", pos.x, pos.y, Color(255, 0, 0), TEXT_ALIGN_CENTER, TEXT_ALIGN_BOTTOM)
+			end
+		end
+	end
+	if settings.playernametag then
+		for _, ent in ipairs(allents) do
+			if ent:IsPlayer() and ent ~= ply and ent:Alive() then
+				local pos = ent:EyePos():ToScreen()
+				draw.SimpleText(ent:Nick(), "BudgetLabel", pos.x, pos.y, Color(255, 255, 0), TEXT_ALIGN_CENTER, TEXT_ALIGN_BOTTOM)
+			end
 		end
 	end
 end)
 
-hook.Add("HUDPaint", "Speedometer", function()
+hook.Add("HUDPaint", "DrawSpeedometer", function()
 	local ply = LocalPlayer()
 	local speed = math.Round(ply:GetVelocity():Length())
 	if settings.speedometer and ply:Alive() then
@@ -47,10 +74,36 @@ end)
 
 hook.Add("CreateMove", "AutoBhop", function(cmd)
 	local ply = LocalPlayer()
-	if settings.autobhop and cmd:KeyDown(IN_JUMP) and not ply:IsOnGround() or ply:WaterLevel() > 1 or ply:GetMoveType() == MOVETYPE_NOCLIP then
+	if settings.autobhop and cmd:KeyDown(IN_JUMP) and not ply:IsOnGround() and ply:WaterLevel() <= 1 and ply:GetMoveType() ~= MOVETYPE_NOCLIP then
 		cmd:RemoveKey(IN_JUMP)
 	end
 end)
+
+hook.Add("PostDrawTranslucentRenderables", "DrawCursorLines", function()
+	local allents = ents.GetAll()
+	local ply = LocalPlayer()
+	local eyepos = ply:EyePos()
+	local aimvec = ply:GetAimVector()
+	local startpos = eyepos + aimvec * 50
+	if settings.npccursorline then
+		for _, ent in ipairs(allents) do
+			if ent:IsNPC() and ent:Alive() then
+				local endPos = ent:GetPos() + Vector(0, 0, ent:OBBMaxs().z * 0.75)
+				render.DrawLine(startpos, endPos, Color(255, 0, 0), false)
+			end
+		end
+	end
+	if settings.playercursorline then 
+		for _, ent in ipairs(allents) do
+			if ent:IsPlayer() and ent:Alive() and ent ~= ply then
+				local endPos = ent:GetPos() + Vector(0, 0, ent:OBBMaxs().z * 0.75)
+				render.DrawLine(startpos, endPos, Color(255, 255, 0), false)
+			end
+		end
+	end
+end)
+
+-- #$%#$% MENU #$%#$%
 
 local function createlabel(text, parent)
 	local label = vgui.Create("DLabel", parent)
@@ -95,16 +148,19 @@ local function createmenu()
 	createcheckbox("Auto Bhop", scrollutility, "autobhop")
 	createlabel("Miscellaneous Options:", scrolldisplay)
 	createcheckbox("Speedometer", scrolldisplay, "speedometer")
-	createlabel("Prop Options:", scrolldisplay)
 	createcheckbox("Prop Box", scrolldisplay, "propbox")
 	createlabel("NPC Options:", scrolldisplay)
 	createcheckbox("NPC Box", scrolldisplay, "npcbox")
 	createcheckbox("NPC Nametag", scrolldisplay, "npcnametag")
+	createcheckbox("NPC Cursor Line", scrolldisplay, "npccursorline")
 	createlabel("Player Options:", scrolldisplay)
 	createcheckbox("Player Box", scrolldisplay, "playerbox")
 	createcheckbox("Player Nametag", scrolldisplay, "playernametag")
+	createcheckbox("Player Cursor Line", scrolldisplay, "playercursorline")
 	return frame
 end
+
+-- #$%#$% COMMANDS #$%#$%
 
 concommand.Add("open_utility_menu", function()
 	if IsValid(utilitymenu) then
