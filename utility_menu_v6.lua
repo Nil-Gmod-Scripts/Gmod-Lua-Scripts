2bd04f09f94dce1d09db3068382a101e28d51172
simple-colour <simply.known.colour@gmail.com>
2025-08-11 19:38:04 +1000

    Add files via upload

---
+++ utility_menu_v6.lua
@@ -0,0 +1,123 @@
+if not settings then
+	settings = {}
+end
+
+hook.Add("CreateMove", "autobhop", function(cmd)
+	local ply = LocalPlayer()
+	if settings.autobhop and cmd:KeyDown(IN_JUMP) and not ply:IsOnGround() and ply:WaterLevel() <= 1 and ply:GetMoveType() ~= MOVETYPE_NOCLIP then
+		cmd:RemoveKey(IN_JUMP)
+	end
+end)
+
+hook.Add("HUDPaint", "drawclientinfo", function()
+	local ply = LocalPlayer()
+		local sw, sh = ScrW(), ScrH()
+	if settings.clientinfo and ply:Alive() then
+		draw.SimpleText("Speed:" .. math.Round(ply:GetVelocity():Length()) .. "u/s", "BudgetLabel", sw / 2, sh / 2 + 75, Color(255, 255, 255), TEXT_ALIGN_CENTER)
+		draw.SimpleText("FPS:" .. math.floor(1 / FrameTime()), "BudgetLabel", sw / 2, sh / 2 + 87, Color(255, 255, 255), TEXT_ALIGN_CENTER)
+	end
+end)
+
+hook.Add("PostDrawOpaqueRenderables", "drawentityboxes", function()
+	if settings.propbox or settings.npcbox or settings.playerbox then
+		for _, ent in ipairs(ents.GetAll()) do
+			if IsValid(ent) then
+				local pos = ent:GetPos()
+				local obbMins, obbMaxs = ent:OBBMins(), ent:OBBMaxs()
+				if settings.propbox and ent:GetClass():lower():find("prop_") then
+					render.DrawWireframeBox(pos, ent:GetAngles(), obbMins, obbMaxs, Color(0, 255, 255), false)
+				elseif ent:Alive() then
+					if settings.npcbox and ent:IsNPC() then
+						render.DrawWireframeBox(pos, Angle(0, 0, 0), obbMins, obbMaxs, Color(255, 155, 0), false)
+					elseif settings.playerbox and ent:IsPlayer() and ent ~= LocalPlayer() then
+						render.DrawWireframeBox(pos, Angle(0, 0, 0), obbMins, obbMaxs, Color(255, 255, 0), false)
+					end
+				end
+			end
+		end
+	end
+end)
+
+hook.Add("HUDPaint", "drawentityinfo", function()
+	if settings.npcinfo or settings.playerinfo then
+		for _, ent in ipairs(ents.GetAll()) do
+			if IsValid(ent) then
+				local pos = ent:EyePos():ToScreen()
+				local health = ent:Health()
+				if ent:Alive() then
+					if settings.npcinfo and ent:IsNPC() then
+						draw.SimpleText(ent:GetClass(), "BudgetLabel", pos.x, pos.y + -12, Color(255, 155, 0), TEXT_ALIGN_CENTER, TEXT_ALIGN_BOTTOM)
+						draw.SimpleText("HP:" .. health, "BudgetLabel", pos.x, pos.y, Color(255, 155, 0), TEXT_ALIGN_CENTER, TEXT_ALIGN_BOTTOM)
+					elseif settings.playerinfo and ent:IsPlayer() and ent ~= LocalPlayer() then
+						draw.SimpleText(ent:Nick(), "BudgetLabel", pos.x, pos.y + -12, Color(255, 255, 0), TEXT_ALIGN_CENTER, TEXT_ALIGN_BOTTOM)
+						draw.SimpleText("HP:" .. health .. "|AP:" .. ent:Armor(), "BudgetLabel", pos.x, pos.y, Color(255, 255, 0), TEXT_ALIGN_CENTER, TEXT_ALIGN_BOTTOM)
+					end
+				end
+			end
+		end
+	end
+end)
+
+local function createlabel(text, parent)
+	local label = vgui.Create("DLabel", parent)
+	label:SetText(text)
+	label:SetFont("DermaDefaultBold")
+	label:SetTextColor(color_white)
+	label:SizeToContents()
+	label:Dock(TOP)
+	label:DockMargin(5, 5, 0, 0)
+	return label
+end
+
+local function createcheckbox(text, parent, key)
+	local checkbox = vgui.Create("DCheckBoxLabel", parent)
+	checkbox:SetText(text)
+	checkbox:SetFont("DermaDefault")
+	checkbox:SetTextColor(color_white)
+	checkbox:SetValue(settings[key] and true or false)
+	checkbox:Dock(TOP)
+	checkbox:SizeToContents()
+	checkbox:DockMargin(10, 5, 0, 0)
+	checkbox.OnChange = function(self, val) settings[key] = val and true or false end
+	return checkbox
+end
+
+local function createmenu()
+	local frame = vgui.Create("DFrame")
+	local tab = vgui.Create("DPropertySheet", frame)
+	local scrollutility = vgui.Create("DScrollPanel", tab)
+	local scrolldisplay = vgui.Create("DScrollPanel", tab)
+	frame:SetSize(300, 400)
+	frame:Center()
+	frame:SetTitle("Utility Menu V6")
+	frame:SetDeleteOnClose(false)
+	frame:SetVisible(false)
+	tab:Dock(FILL)
+	tab:SetFadeTime(0)
+	tab:AddSheet("Utility", scrollutility, "icon16/wrench.png")
+	tab:AddSheet("Display", scrolldisplay, "icon16/monitor.png")
+	createlabel("Miscellaneous Options:", scrollutility)
+	createcheckbox("Auto Bhop", scrollutility, "autobhop")
+	createlabel("Miscellaneous Options:", scrolldisplay)
+	createcheckbox("Draw Client Info", scrolldisplay, "clientinfo")
+	createcheckbox("Draw Prop Boxes", scrolldisplay, "propbox")
+	createlabel("NPC Options:", scrolldisplay)
+	createcheckbox("Draw NPC Boxes", scrolldisplay, "npcbox")
+	createcheckbox("Draw NPC Info", scrolldisplay, "npcinfo")
+	createlabel("Player Options:", scrolldisplay)
+	createcheckbox("Draw Player Boxes", scrolldisplay, "playerbox")
+	createcheckbox("Draw Player Info", scrolldisplay, "playerinfo")
+	return frame
+end
+
+concommand.Remove("open_utility_menu")
+concommand.Add("open_utility_menu", function()
+	if IsValid(utilitymenu) then
+		utilitymenu:SetVisible(true)
+		utilitymenu:MakePopup()
+	else
+		utilitymenu = createmenu()
+		utilitymenu:SetVisible(true)
+		utilitymenu:MakePopup()
+	end
+end)
